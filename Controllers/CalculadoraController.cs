404d12932e10e419fc05c26f09021ab3e333846d
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2023-08-09 18:29:21 -0300

    adding methods calculator

---
+++ Controllers/CalculadoraController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestComAspNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculadoraController : ControllerBase
+    {
+        private readonly ILogger<CalculadoraController> _logger;
+
+        public CalculadoraController(ILogger<CalculadoraController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("adicao/{primeiroNumero}/{segundoNumero}")]
+        public IActionResult GetAdicao(string primeiroNumero, string segundoNumero)
+        {
+            if (IsNumeric(primeiroNumero) && IsNumeric(segundoNumero))
+            {
+                var soma = ConvertToDecimal(primeiroNumero) + ConvertToDecimal(segundoNumero);
+                return Ok(soma.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("subtracao/{primeiroNumero}/{segundoNumero}")]
+        public IActionResult GetSubtracao(string primeiroNumero, string segundoNumero)
+        {
+            if (IsNumeric(primeiroNumero) && IsNumeric(segundoNumero))
+            {
+                var soma = ConvertToDecimal(primeiroNumero) - ConvertToDecimal(segundoNumero);
+                return Ok(soma.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("divisao/{primeiroNumero}/{segundoNumero}")]
+        public IActionResult GetDivisao(string primeiroNumero, string segundoNumero)
+        {
+            if (IsNumeric(primeiroNumero) && IsNumeric(segundoNumero))
+            {
+                var soma = ConvertToDecimal(primeiroNumero) / ConvertToDecimal(segundoNumero);
+                return Ok(soma.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("multiplicacao/{primeiroNumero}/{segundoNumero}")]
+        public IActionResult GetMultiplicacao(string primeiroNumero, string segundoNumero)
+        {
+            if (IsNumeric(primeiroNumero) && IsNumeric(segundoNumero))
+            {
+                var soma = ConvertToDecimal(primeiroNumero) * ConvertToDecimal(segundoNumero);
+                return Ok(soma.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+        private bool IsNumeric(string strNumero)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumero, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumero)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(strNumero, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }       
+    }
+}
