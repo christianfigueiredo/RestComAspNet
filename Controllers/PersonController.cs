094dd17ef06c7cc7639a5526962b7fe7539223b4
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2023-08-09 19:15:14 -0300

    criação da pasta Model e classe person

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestComAspNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("adicao/{primeiroNumero}/{segundoNumero}")]
+        public IActionResult Adicao(string primeiroNumero, string segundoNumero)
+        {         
+
+            return BadRequest("Invalid Input");
+        }
+
+       
+        private bool IsNumeric(string strNumero)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumero, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumero)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(strNumero, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }       
+    }
+}
---
+++ Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestComAspNet.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string Sobrenome { get; set;}
+        public string Endereco { get; set; }
+        public string Genero { get; set;}
+    }
+}
