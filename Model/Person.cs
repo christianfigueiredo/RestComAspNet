211b5eccf168a1e536989598d3e097bb5ae6ad20
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2023-08-11 09:39:27 -0300

    mapeamento da entidade

--- Model/Person.cs
+++ Model/Person.cs
@@ -1,11 +1,23 @@
-﻿namespace RestComAspNet.Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestComAspNet.Model
 {
+    [Table("person")]
     public class Person
     {
+        [Column("id")]
         public long Id { get; set; }
+
+        [Column("primeiro_nome")]
         public string PrimeiroNome { get; set; }
+
+        [Column("sobrenono")]
         public string Sobrenome { get; set;}
+
+        [Column("endereco")]
         public string Endereco { get; set; }
+
+        [Column("genero")]
         public string Genero { get; set;}
     }
 }
